a3fe6051b31fc0423bd15f512d99a502fba3dfb6
Mr-K-AB <75632903+Mr-K-AB@users.noreply.github.com>
2024-05-21 00:52:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,17 @@
-function addRecommendation() {
-  // Get the message of the new recommendation
-  let recommendation = document.getElementById("new_recommendation");
-  // If the user has left a recommendation, display a pop-up
-  if (recommendation.value != null && recommendation.value.trim() != "") {
-    console.log("New recommendation added");
-    //Call showPopup here
+// script.js
 
-    // Create a new 'recommendation' element and set it's value to the user's message
-    var element = document.createElement("div");
-    element.setAttribute("class","recommendation");
-    element.innerHTML = "\<span\>&#8220;\</span\>" + recommendation.value + "\<span\>&#8221;\</span\>";
-    // Add this element to the end of the list of recommendations
-    document.getElementById("all_recommendations").appendChild(element); 
-    
-    // Reset the value of the textarea
-    recommendation.value = "";
-  }
-}
+const messengerAppLinkInput = document.getElementById('messenger-app-link');
+const messengerAppThumbnailContainer = document.getElementById('messenger-app-thumbnail');
 
-function showPopup(bool) {
-  if (bool) {
-    document.getElementById('popup').style.visibility = 'visible'
+messengerAppLinkInput.addEventListener('input', () => {
+  const githubLink = messengerAppLinkInput.value;
+
+  if (githubLink) {
+    const repositoryName = githubLink.split('/').pop().replace('.git', '');
+    const thumbnailUrl = `https://opengraph.githubassets.com/1/${repositoryName}`;
+
+    messengerAppThumbnailContainer.innerHTML = `<img src="${thumbnailUrl}" alt="GitHub Repository Thumbnail">`;
   } else {
-    document.getElementById('popup').style.visibility = 'hidden'
+    messengerAppThumbnailContainer.innerHTML = '';
   }
-}
+});
